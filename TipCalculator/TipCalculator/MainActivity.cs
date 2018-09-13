46e8d3687e2087d58a53a2eaf13535de1dfd311c
JaanJahimees <jaanjahilo@outlook.com>
2018-09-13 08:41:34 +0300

    seekbar and textview

--- TipCalculator/TipCalculator/MainActivity.cs
+++ TipCalculator/TipCalculator/MainActivity.cs
@@ -9,11 +9,22 @@ namespace TipCalculator
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView textView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            textView = FindViewById<TextView>(Resource.Id.textView1);
+            var seekBar = FindViewById<SeekBar>(Resource.Id.seekBar1);
+
+            seekBar.ProgressChanged += SeekBar_ProgressChanged;
+        }
+
+        private void SeekBar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            textView.Text = e.Progress.ToString();
         }
     }
 }
