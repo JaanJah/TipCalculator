7a1ae06792a1c9414ecc8c953707b769a425f637
JaanJahimees <jaanjahilo@outlook.com>
2018-09-13 09:13:12 +0300

    save

--- TipCalculator/TipCalculator/MainActivity.cs
+++ TipCalculator/TipCalculator/MainActivity.cs
@@ -9,22 +9,32 @@ namespace TipCalculator
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        TextView textView;
+        TextView tipPercent;
+        EditText subTotalValue;
+        TextView tipAmountValue;
+        TextView totalValue;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            textView = FindViewById<TextView>(Resource.Id.textView1);
+            tipPercent = FindViewById<TextView>(Resource.Id.tipPercent);
             var seekBar = FindViewById<SeekBar>(Resource.Id.seekBar1);
+            subTotalValue = FindViewById<EditText>(Resource.Id.subTotalValue);
+            tipAmountValue = FindViewById<TextView>(Resource.Id.tipAmountValue);
+            totalValue = FindViewById<TextView>(Resource.Id.totalValue);
 
             seekBar.ProgressChanged += SeekBar_ProgressChanged;
+
+            var calculateTip = 0;
+            var calculateTotal = 0;
         }
 
         private void SeekBar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
         {
-            textView.Text = e.Progress.ToString();
+            tipPercent.Text = e.Progress.ToString();
+            return;
         }
     }
 }
